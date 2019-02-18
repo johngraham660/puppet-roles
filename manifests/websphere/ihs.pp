de92ea402e3198418c3c500de4d868e5302de8a4
John Graham <john.graham@virtua-enterprise.co.uk>
2019-02-18 08:13:30 +0000

    Splitting websphere into two roles (was & ihs)

---
+++ manifests/websphere/ihs.pp
@@ -0,0 +1,11 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include roles::websphere::ihs
+class roles::websphere::ihs {
+
+  include profiles::websphere::common
+  include profiles::websphere::ihs
+}
---
+++ manifests/websphere/was.pp
@@ -0,0 +1,12 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include roles::websphere::was
+class roles::websphere::was {
+
+  include profiles::websphere::common
+  include profiles::websphere::was
+
+}
